179073aaca6f432d77d3bc7da7527b51151d5c6b
Harshit Gupta <harshit.mahajan1810@gmail.com>
2022-09-08 11:55:22 -0400

    self-declaration

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+* WEB322 – Assignment 1
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Harshit Gupta Student ID: 157860214 Date: 09/08/2022
+*
+* Online (Cyclic) URL: https://dull-pear-clam-hose.cyclic.app
+*
+********************************************************************************/
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
